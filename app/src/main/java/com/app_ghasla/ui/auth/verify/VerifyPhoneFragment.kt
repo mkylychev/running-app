7e58111ac623a035679ac5fcc6def740eecfb690
mkylychev <mkylychev@gmail.com>
2022-04-26 16:10:46 +0600

    GL-74: mocup virify phone fragment

--- app/src/main/java/com/app_ghasla/ui/auth/verify/VerifyPhoneFragment.kt
+++ app/src/main/java/com/app_ghasla/ui/auth/verify/VerifyPhoneFragment.kt
@@ -1,12 +1,41 @@
 package com.app_ghasla.ui.auth.verify
 
+import android.graphics.Color
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.app_ghasla.R
+import com.app_ghasla.common.extension.setLink
 import com.app_ghasla.databinding.FragmentVerifyPhoneBinding
 import com.app_ghasla.ui.base.fragment.BaseFragment
 
-class VerifyPhoneFragment: BaseFragment<FragmentVerifyPhoneBinding>() {
+class VerifyPhoneFragment : BaseFragment<FragmentVerifyPhoneBinding>() {
 
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentVerifyPhoneBinding
         get() = FragmentVerifyPhoneBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUi()
+    }
+
+    private fun initUi() {
+        setResendTextView()
+    }
+
+    /**
+     * Resend sms code
+     */
+    private fun setResendTextView() = with(binding.tvDoNotReceiveCode) {
+        setLink(
+            getString(R.string.verify_screen_code_do_not_receive_code),
+            getString(R.string.verify_screen_code_do_not_receive_code_resend),
+            requireContext()
+        ) { resendCode() }
+    }
+
+    private fun resendCode() {
+
+    }
 }
